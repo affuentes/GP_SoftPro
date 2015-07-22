0d134ada4a2105fdcdf176658affe7662dd7663e
edison -edison <edisonperez@misena.edu.co>
2015-07-21 20:53:29 -0500

    Clinica

--- Clinica/Clinica/Citas.Designer.cs
+++ Clinica/Clinica/Citas.Designer.cs
@@ -28,11 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Citas));
+            this.Reportes = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.Salir = new System.Windows.Forms.Button();
+            this.Reportes.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Reportes
+            // 
+            this.Reportes.BackColor = System.Drawing.Color.SteelBlue;
+            this.Reportes.Controls.Add(this.panel4);
+            this.Reportes.Controls.Add(this.label1);
+            this.Reportes.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Reportes.Location = new System.Drawing.Point(0, 0);
+            this.Reportes.Name = "Reportes";
+            this.Reportes.Size = new System.Drawing.Size(1378, 88);
+            this.Reportes.TabIndex = 25;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.label2);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel4.Location = new System.Drawing.Point(1027, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(351, 88);
+            this.panel4.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(0, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 33);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(3, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 73);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Citas";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.SteelBlue;
+            this.panel3.Controls.Add(this.Salir);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(0, 702);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1378, 78);
+            this.panel3.TabIndex = 24;
+            // 
+            // Salir
+            // 
+            this.Salir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Salir.Dock = System.Windows.Forms.DockStyle.Right;
+            this.Salir.FlatAppearance.BorderSize = 0;
+            this.Salir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Salir.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Salir.ForeColor = System.Drawing.Color.White;
+            this.Salir.Image = ((System.Drawing.Image)(resources.GetObject("Salir.Image")));
+            this.Salir.Location = new System.Drawing.Point(1288, 0);
+            this.Salir.Name = "Salir";
+            this.Salir.Size = new System.Drawing.Size(90, 78);
+            this.Salir.TabIndex = 12;
+            this.Salir.Text = "Salir";
+            this.Salir.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Salir.UseVisualStyleBackColor = true;
+            // 
+            // Citas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1378, 780);
+            this.Controls.Add(this.Reportes);
+            this.Controls.Add(this.panel3);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Citas";
             this.Text = "Citas";
+            this.Reportes.ResumeLayout(false);
+            this.Reportes.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        public System.Windows.Forms.Panel Reportes;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label2;
+        public System.Windows.Forms.Label label1;
+        public System.Windows.Forms.Panel panel3;
+        public System.Windows.Forms.Button Salir;
     }
 }
--- Clinica/Clinica/Form1.Designer.cs
+++ Clinica/Clinica/Form1.Designer.cs
@@ -33,13 +33,13 @@
             this.Centro_acopio = new System.Windows.Forms.Button();
             this.Reportes = new System.Windows.Forms.Panel();
             this.panel4 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.Empleado = new System.Windows.Forms.Button();
             this.Cliente = new System.Windows.Forms.Button();
             this.panel3 = new System.Windows.Forms.Panel();
             this.Salir = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.label2 = new System.Windows.Forms.Label();
             this.Reportes.SuspendLayout();
             this.panel4.SuspendLayout();
             this.panel3.SuspendLayout();
@@ -68,17 +68,28 @@
             this.Reportes.Dock = System.Windows.Forms.DockStyle.Top;
             this.Reportes.Location = new System.Drawing.Point(0, 0);
             this.Reportes.Name = "Reportes";
-            this.Reportes.Size = new System.Drawing.Size(1386, 88);
+            this.Reportes.Size = new System.Drawing.Size(1378, 88);
             this.Reportes.TabIndex = 23;
             // 
             // panel4
             // 
             this.panel4.Controls.Add(this.label2);
             this.panel4.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel4.Location = new System.Drawing.Point(1035, 0);
+            this.panel4.Location = new System.Drawing.Point(1027, 0);
             this.panel4.Name = "panel4";
             this.panel4.Size = new System.Drawing.Size(351, 88);
             this.panel4.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(0, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 33);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "label2";
             // 
             // label1
             // 
@@ -128,9 +139,9 @@
             this.panel3.BackColor = System.Drawing.Color.SteelBlue;
             this.panel3.Controls.Add(this.Salir);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel3.Location = new System.Drawing.Point(0, 710);
+            this.panel3.Location = new System.Drawing.Point(0, 702);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(1386, 78);
+            this.panel3.Size = new System.Drawing.Size(1378, 78);
             this.panel3.TabIndex = 22;
             // 
             // Salir
@@ -142,7 +153,7 @@
             this.Salir.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Salir.ForeColor = System.Drawing.Color.White;
             this.Salir.Image = ((System.Drawing.Image)(resources.GetObject("Salir.Image")));
-            this.Salir.Location = new System.Drawing.Point(1296, 0);
+            this.Salir.Location = new System.Drawing.Point(1288, 0);
             this.Salir.Name = "Salir";
             this.Salir.Size = new System.Drawing.Size(90, 78);
             this.Salir.TabIndex = 12;
@@ -157,23 +168,12 @@
             this.timer1.Interval = 200;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(0, 31);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(93, 33);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "label2";
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1386, 788);
+            this.ClientSize = new System.Drawing.Size(1378, 780);
             this.Controls.Add(this.Centro_acopio);
             this.Controls.Add(this.Reportes);
             this.Controls.Add(this.Empleado);

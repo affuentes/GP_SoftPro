55b4681e43cac7c5dfc710583f2a2f7faae4cf6b
edison20 <edisonperez@misena.edu.co>
2015-08-14 20:51:57 -0500

    Clinica

--- Clinica/Clinica/Doctores.Designer.cs
+++ Clinica/Clinica/Doctores.Designer.cs
@@ -31,6 +31,12 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Doctores));
             this.Salir = new System.Windows.Forms.Button();
             this.panel3 = new System.Windows.Forms.Panel();
+            this.BNuevo = new System.Windows.Forms.Button();
+            this.BBuscar = new System.Windows.Forms.Button();
+            this.BEliminar = new System.Windows.Forms.Button();
+            this.BCancelar = new System.Windows.Forms.Button();
+            this.BEditar = new System.Windows.Forms.Button();
+            this.BGuardar = new System.Windows.Forms.Button();
             this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -62,12 +68,126 @@
             this.panel3.Size = new System.Drawing.Size(2744, 78);
             this.panel3.TabIndex = 23;
             // 
+            // BNuevo
+            // 
+            this.BNuevo.BackColor = System.Drawing.Color.SteelBlue;
+            this.BNuevo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BNuevo.FlatAppearance.BorderSize = 0;
+            this.BNuevo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BNuevo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BNuevo.ForeColor = System.Drawing.Color.White;
+            this.BNuevo.Image = ((System.Drawing.Image)(resources.GetObject("BNuevo.Image")));
+            this.BNuevo.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.BNuevo.Location = new System.Drawing.Point(968, 390);
+            this.BNuevo.Name = "BNuevo";
+            this.BNuevo.Size = new System.Drawing.Size(87, 97);
+            this.BNuevo.TabIndex = 24;
+            this.BNuevo.Text = "Nuevo";
+            this.BNuevo.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.BNuevo.UseVisualStyleBackColor = false;
+            // 
+            // BBuscar
+            // 
+            this.BBuscar.BackColor = System.Drawing.Color.SteelBlue;
+            this.BBuscar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BBuscar.FlatAppearance.BorderSize = 0;
+            this.BBuscar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BBuscar.ForeColor = System.Drawing.Color.White;
+            this.BBuscar.Image = ((System.Drawing.Image)(resources.GetObject("BBuscar.Image")));
+            this.BBuscar.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.BBuscar.Location = new System.Drawing.Point(1399, 390);
+            this.BBuscar.Name = "BBuscar";
+            this.BBuscar.Size = new System.Drawing.Size(87, 97);
+            this.BBuscar.TabIndex = 28;
+            this.BBuscar.Text = "Buscar";
+            this.BBuscar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.BBuscar.UseVisualStyleBackColor = false;
+            // 
+            // BEliminar
+            // 
+            this.BEliminar.BackColor = System.Drawing.Color.SteelBlue;
+            this.BEliminar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BEliminar.FlatAppearance.BorderSize = 0;
+            this.BEliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BEliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BEliminar.ForeColor = System.Drawing.Color.White;
+            this.BEliminar.Image = ((System.Drawing.Image)(resources.GetObject("BEliminar.Image")));
+            this.BEliminar.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.BEliminar.Location = new System.Drawing.Point(1509, 390);
+            this.BEliminar.Name = "BEliminar";
+            this.BEliminar.Size = new System.Drawing.Size(87, 97);
+            this.BEliminar.TabIndex = 29;
+            this.BEliminar.Text = "Eliminar";
+            this.BEliminar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.BEliminar.UseVisualStyleBackColor = false;
+            // 
+            // BCancelar
+            // 
+            this.BCancelar.BackColor = System.Drawing.Color.SteelBlue;
+            this.BCancelar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BCancelar.FlatAppearance.BorderSize = 0;
+            this.BCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BCancelar.ForeColor = System.Drawing.Color.White;
+            this.BCancelar.Image = ((System.Drawing.Image)(resources.GetObject("BCancelar.Image")));
+            this.BCancelar.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.BCancelar.Location = new System.Drawing.Point(1179, 390);
+            this.BCancelar.Name = "BCancelar";
+            this.BCancelar.Size = new System.Drawing.Size(87, 97);
+            this.BCancelar.TabIndex = 26;
+            this.BCancelar.Text = "Cancelar";
+            this.BCancelar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.BCancelar.UseVisualStyleBackColor = false;
+            // 
+            // BEditar
+            // 
+            this.BEditar.BackColor = System.Drawing.Color.SteelBlue;
+            this.BEditar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BEditar.FlatAppearance.BorderSize = 0;
+            this.BEditar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BEditar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BEditar.ForeColor = System.Drawing.Color.White;
+            this.BEditar.Image = ((System.Drawing.Image)(resources.GetObject("BEditar.Image")));
+            this.BEditar.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.BEditar.Location = new System.Drawing.Point(1289, 390);
+            this.BEditar.Name = "BEditar";
+            this.BEditar.Size = new System.Drawing.Size(87, 97);
+            this.BEditar.TabIndex = 27;
+            this.BEditar.Text = "Editar";
+            this.BEditar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.BEditar.UseVisualStyleBackColor = false;
+            // 
+            // BGuardar
+            // 
+            this.BGuardar.BackColor = System.Drawing.Color.SteelBlue;
+            this.BGuardar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BGuardar.FlatAppearance.BorderSize = 0;
+            this.BGuardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BGuardar.ForeColor = System.Drawing.Color.White;
+            this.BGuardar.Image = ((System.Drawing.Image)(resources.GetObject("BGuardar.Image")));
+            this.BGuardar.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.BGuardar.Location = new System.Drawing.Point(1069, 390);
+            this.BGuardar.Name = "BGuardar";
+            this.BGuardar.Size = new System.Drawing.Size(87, 97);
+            this.BGuardar.TabIndex = 25;
+            this.BGuardar.Text = "Guardar";
+            this.BGuardar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.BGuardar.UseVisualStyleBackColor = false;
+            // 
             // Doctores
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(2744, 780);
+            this.Controls.Add(this.BNuevo);
+            this.Controls.Add(this.BBuscar);
+            this.Controls.Add(this.BEliminar);
+            this.Controls.Add(this.BCancelar);
+            this.Controls.Add(this.BEditar);
+            this.Controls.Add(this.BGuardar);
             this.Controls.Add(this.panel3);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Doctores";
@@ -82,5 +202,11 @@
 
         public System.Windows.Forms.Button Salir;
         public System.Windows.Forms.Panel panel3;
+        public System.Windows.Forms.Button BNuevo;
+        public System.Windows.Forms.Button BBuscar;
+        public System.Windows.Forms.Button BEliminar;
+        public System.Windows.Forms.Button BCancelar;
+        public System.Windows.Forms.Button BEditar;
+        public System.Windows.Forms.Button BGuardar;
     }
 }
